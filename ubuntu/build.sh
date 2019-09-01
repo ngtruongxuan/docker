a53ede5e67ce60bbba5ab7348d25017375a5da87
Truong Xuan <xuan.nguyen@seldatinc.com>
2019-09-01 17:52:24 +0700

    up

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker pull ubuntu:16.04 &&
 docker-compose -f docker-build.yml build supervisor nodejs8 php7.1-nginx php7.1-nginx-mongo php7.2-nginx php7.2-nginx-mongo php7.3-nginx php7.3-nginx-mongo &&
-docker push gitlab.hub.seldatdirect.com/docker-image/ubuntu16:supervisor &&
-docker push gitlab.hub.seldatdirect.com/docker-image/ubuntu16:nodejs8 &&
-docker push gitlab.hub.seldatdirect.com/docker-image/ubuntu16:php7.1-nginx &&
-docker push gitlab.hub.seldatdirect.com/docker-image/ubuntu16:php7.1-nginx-mongo &&
-docker push gitlab.hub.seldatdirect.com/docker-image/ubuntu16:php7.2-nginx &&
-docker push gitlab.hub.seldatdirect.com/docker-image/ubuntu16:php7.2-nginx-mongo &&
-docker push gitlab.hub.seldatdirect.com/docker-image/ubuntu16:php7.3-nginx &&
-docker push gitlab.hub.seldatdirect.com/docker-image/ubuntu16:php7.3-nginx-mongo
+docker push github.com/ngtruongxuan/docker.git#:ubuntu/build/supervisor &&
+docker push github.com/ngtruongxuan/docker.git#:ubuntu/build/nodejs8 &&
+docker push github.com/ngtruongxuan/docker.git#:ubuntu/build/php7.1-nginx &&
+docker push github.com/ngtruongxuan/docker.git#:ubuntu/build/php7.1-nginx-mongo &&
+docker push github.com/ngtruongxuan/docker.git#:ubuntu/build/php7.2-nginx &&
+docker push github.com/ngtruongxuan/docker.git#:ubuntu/build/php7.2-nginx-mongo &&
+docker push github.com/ngtruongxuan/docker.git#:ubuntu/build/php7.3-nginx &&
+docker push github.com/ngtruongxuan/docker.git#:ubuntu/build/php7.3-nginx-mongo
